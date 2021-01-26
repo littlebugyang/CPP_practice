97c917ee6464d8051d298f4043223e5d92c55096
littlebugyang <im.lby@qq.com>
2021-01-26 23:54:37 +0800

    Hello World.

---
+++ cpp_primer_plus/myfirst.cpp
@@ -0,0 +1,13 @@
+/*
+ * @Date: 2021-01-26 23:53:18
+ * @LastEditors: lby
+ */
+#include <iostream>
+int main()
+{
+  using namespace std;
+  cout << "Come up and C++ me some time.";
+  cout << endl;
+  cout << "You won't regret it!" << endl;
+  return 0;
+}
