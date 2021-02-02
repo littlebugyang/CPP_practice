ea668426a1c4a3a8db4a4372e4930ef7858c810a
littlebugyang <im.lby@qq.com>
2021-02-03 00:30:44 +0800

    Talking about float-point types, add fltadd.cpp and floatnum.cpp

---
+++ cpp_primer_plus/floatnum.cpp
@@ -0,0 +1,20 @@
+/*
+ * @Date: 2021-02-03 00:05:37
+ * @LastEditors: lby
+ */
+#include <iostream>
+int main()
+{
+  using namespace std;
+  cout.setf(ios_base::fixed, ios_base::floatfield); // fixed-point
+  float tub = 10.0 / 3.0;                           // good to about 6 places
+  double mint = 10.0 / 3.0;                         // good to about 15 places
+  const float million = 1.0e6;
+  cout << "tub = " << tub;
+  cout << ", a million tubs = " << million * tub;
+  cout << ",\nand ten million tubs = ";
+  cout << 10 * million * tub << endl;
+  cout << "mint = " << mint << " and a million mints = ";
+  cout << million * mint << endl;
+  return 0;
+}
---
+++ cpp_primer_plus/fltadd.cpp
@@ -0,0 +1,15 @@
+/*
+ * @Date: 2021-02-03 00:18:18
+ * @LastEditors: lby
+ */
+#include <iostream>
+int main()
+{
+  using namespace std;
+  float a = 2.34E+22f;
+  float b = a + 1.0f;
+
+  cout << "a = " << a << endl;
+  cout << "b - a = " << b - a << endl;
+  return 0;
+}
