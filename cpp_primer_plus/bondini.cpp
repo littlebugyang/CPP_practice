062d9f7f0ba66f51cc3b89fc3cc50b79a3d4d872
littlebugyang <im.lby@qq.com>
2021-02-02 17:41:36 +0800

    Talking about escape, add bondini.cpp.

---
+++ cpp_primer_plus/bondini.cpp
@@ -0,0 +1,16 @@
+/*
+ * @Date: 2021-02-02 17:39:08
+ * @LastEditors: lby
+ */
+#include <iostream>
+int main()
+{
+  using namespace std;
+  cout << "\aOperation \"HyperHype\" is now activated!\n";
+  cout << "Enter your agent code:________\b\b\b\b\b\b\b\b";
+  long code;
+  cin >> code;
+  cout << "\aYou entered " << code << "...\n";
+  cout << "\aCode verified! Proceed with Plan Z3!\n";
+  return 0;
+}
