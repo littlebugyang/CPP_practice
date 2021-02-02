739a6d88b507afbe4ffe11d1f6f68a1453a91a19
littlebugyang <im.lby@qq.com>
2021-02-02 13:41:58 +0800

    Talking about char, add chartype.cpp and morechar.cpp

---
+++ cpp_primer_plus/chartype.cpp
@@ -0,0 +1,16 @@
+/*
+ * @Date: 2021-02-02 13:32:09
+ * @LastEditors: lby
+ */
+#include <iostream>
+int main()
+{
+  using namespace std;
+  char ch;
+
+  cout << "Enter a character: " << endl;
+  cin >> ch;
+  cout << "Hola! ";
+  cout << "Thank you for the " << ch << " character." << endl;
+  return 0;
+}
