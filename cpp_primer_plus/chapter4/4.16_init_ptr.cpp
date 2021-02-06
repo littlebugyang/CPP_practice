af11ecfe173e76f6fa0988dc1814bd5f3037f7bf
littlebugyang <im.lby@qq.com>
2021-02-07 00:55:44 +0800

    Complement.

--- cpp_primer_plus/chapter4/4.16_init_ptr.cpp
+++ cpp_primer_plus/chapter4/4.16_init_ptr.cpp
@@ -2,3 +2,15 @@
  * @Date: 2021-02-07 00:39:54
  * @LastEditors: lby
  */
+#include <iostream>
+int main()
+{
+  using namespace std;
+  int higgens = 5;
+  int *pt = &higgens;
+  cout << "Value of higgens = " << higgens
+       << "; Address of higgens = " << &higgens << endl;
+  cout << "Value of *pt = " << *pt
+       << "; Value of pt = " << pt << endl;
+  return 0;
+}
