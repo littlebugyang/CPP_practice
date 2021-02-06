b9dcbc532f54e39cada9b40148883050db9b7488
littlebugyang <im.lby@qq.com>
2021-02-06 16:26:11 +0800

    Talking about structure, add structur.cpp, assgin_st.cpp, arrstruc.cpp

---
+++ cpp_primer_plus/chapter4/assgin_st.cpp
@@ -0,0 +1,27 @@
+/*
+ * @Date: 2021-02-06 16:12:49
+ * @LastEditors: lby
+ */
+#include <iostream>
+struct inflatable
+{
+  char name[20];
+  float volume;
+  double price;
+};
+int main()
+{
+  using namespace std;
+  inflatable bouquet =
+      {
+          "sunflowers",
+          0.20,
+          12.49};
+  inflatable choice;
+  cout << "bouquet: " << bouquet.name << " for $";
+  cout << bouquet.price << endl;
+  choice = bouquet;
+  cout << "choice: " << choice.name << " for $";
+  cout << choice.price << endl;
+  return 0;
+}
