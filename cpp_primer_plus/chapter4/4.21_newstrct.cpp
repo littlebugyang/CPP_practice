52c7690db48c11a7a9b1babfac73fe7843000de3
littlebugyang <im.lby@qq.com>
2021-02-11 00:14:09 +0800

    Talking about pointer to structure, add newstrct.cpp

---
+++ cpp_primer_plus/chapter4/4.21_newstrct.cpp
@@ -0,0 +1,27 @@
+/*
+ * @Date: 2021-02-11 00:10:56
+ * @LastEditors: lby
+ */
+#include <iostream>
+struct inflatable
+{
+  char name[20];
+  float volume;
+  double price;
+};
+int main()
+{
+  using namespace std;
+  inflatable *ps = new inflatable;
+  cout << "Enter name of inflatable item: ";
+  cin.get(ps->name, 20);
+  cout << "Enter volume in cubic feet: ";
+  cin >> (*ps).volume;
+  cout << "Enter price: $";
+  cin >> ps->price;
+  cout << "Name: " << (*ps).name << endl;
+  cout << "Volume: " << ps->volume << " cubic feet\n";
+  cout << "Price: $" << ps->price << endl;
+  delete ps;
+  return 0;
+}
