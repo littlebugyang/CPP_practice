72535e4f309cfaea12afed60d4a765fa2d8faf8c
littlebugyang <im.lby@qq.com>
2021-02-12 01:35:31 +0800

    Talking about new and delete, add delete.cpp

---
+++ cpp_primer_plus/chapter4/4.22_delete.cpp
@@ -0,0 +1,33 @@
+/*
+ * @Date: 2021-02-11 00:17:22
+ * @LastEditors: lby
+ */
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+char *getname(void);
+
+int main()
+{
+  char *name;
+
+  name = getname();
+  cout << name << " at " << (int *)name << "\n";
+  delete[] name;
+
+  name = getname();
+  cout << name << " at " << (int *)name << "\n";
+  delete[] name;
+  return 0;
+}
+
+char *getname()
+{
+  char temp[80];
+  cout << "Enter last name: ";
+  cin >> temp;
+  char *pn = new char[strlen(temp) + 1];
+  strcpy(pn, temp);
+  return pn;
+}
