b14aea5c110336884d05b8f25e2ea6de4b6570a5
littlebugyang <im.lby@qq.com>
2021-02-20 18:03:17 +0800

    Talking about comma operator, add forstr2.cpp

---
+++ cpp_primer_plus/chapter5/5.9_forstr2.cpp
@@ -0,0 +1,24 @@
+/*
+ * @Date: 2021-02-20 16:01:00
+ * @LastEditors: lby
+ */
+#include <iostream>
+#include <string>
+int main()
+{
+  using namespace std;
+  cout << "Enter a word: ";
+  string word;
+  cin >> word;
+
+  char temp;
+  int i, j;
+  for (j = 0, i = word.size() - 1; j < i; --i, ++j)
+  {
+    temp = word[i];
+    word[i] = word[j];
+    word[j] = temp;
+  }
+  cout << word << "\nDone\n";
+  return 0;
+}
