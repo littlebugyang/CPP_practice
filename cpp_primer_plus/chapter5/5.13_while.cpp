865f8f1d262cbdce511c0586c581cf8ddcc5d100
littlebugyang <im.lby@qq.com>
2021-02-22 04:13:53 +0800

    Talking about while loop, add while.cpp

---
+++ cpp_primer_plus/chapter5/5.13_while.cpp
@@ -0,0 +1,21 @@
+/*
+ * @Date: 2021-02-22 03:47:07
+ * @LastEditors: lby
+ */
+#include <iostream>
+const int ArSize = 20;
+int main()
+{
+  using namespace std;
+  char name[ArSize];
+  cout << "Your first name, please: ";
+  cin >> name;
+  cout << "Here is your name, verticalized and ASCIIized:\n";
+  int i = 0;
+  while (name[i] != '\0')
+  {
+    cout << name[i] << ": " << int(name[i]) << endl;
+    i++;
+  }
+  return 0;
+}
