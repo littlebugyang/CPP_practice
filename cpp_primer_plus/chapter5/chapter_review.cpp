b9c6e83e1928efd6d37aa3e6776dabce0a71c3c0
littlebugyang <im.lby@qq.com>
2021-02-23 20:58:13 +0800

    Add chapter_review for review section of chapter5.

---
+++ cpp_primer_plus/chapter5/chapter_review.cpp
@@ -0,0 +1,18 @@
+/*
+ * @Date: 2021-02-22 22:58:37
+ * @LastEditors: lby
+ */
+#include <iostream>
+
+int main()
+{
+  using namespace std;
+  int x = (1, 024);
+
+  int y;
+  y = 1, 024;
+
+  cout << x << endl;
+  cout << y << endl;
+  return 0;
+}
