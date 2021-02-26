3594dab4d3839d57bc4363abeb9028cb7f1b49c0
littlebugyang <im.lby@qq.com>
2021-02-26 23:31:07 +0800

    Talking about numeric-reading loops, add cinfinish.cpp and cingolf.cpp

--- cpp_primer_plus/chapter6/6.12_jump.cpp
+++ cpp_primer_plus/chapter6/6.12_jump.cpp
@@ -1,24 +1,30 @@
+/*
+ * @Date: 2021-02-26 18:11:09
+ * @LastEditors: lby
+ */
 #include <iostream>
 const int ArSize = 80;
 int main()
 {
-using namespace std;
-char line[ArSize];
-int spaces = 0;
-cout << "Enter a line of text:\n";
-cin.get(line, ArSize);
-cout << "Complete line:\n" << line << endl;
-cout << "Line through first period:\n";
-for (int i = 0; line[i] != '\0'; i++)
-{
-cout << line[i];
-if (line[i] == '.')
-break;
-if (line[i] != ' ')
-continue;
-spaces++;
-}
-cout << "\n" << spaces << " spaces\n";
-cout << "Done.\n";
-return 0;
+  using namespace std;
+  char line[ArSize];
+  int spaces = 0;
+  cout << "Enter a line of text:\n";
+  cin.get(line, ArSize);
+  cout << "Complete line:\n"
+       << line << endl;
+  cout << "Line through first period:\n";
+  for (int i = 0; line[i] != '\0'; i++)
+  {
+    cout << line[i];
+    if (line[i] == '.')
+      break;
+    if (line[i] != ' ')
+      continue;
+    spaces++;
+  }
+  cout << "\n"
+       << spaces << " spaces\n";
+  cout << "Done.\n";
+  return 0;
 }
--- cpp_primer_plus/chapter6/6.9_condit.cpp
+++ cpp_primer_plus/chapter6/6.9_condit.cpp
@@ -1,12 +1,16 @@
+/*
+ * @Date: 2021-02-26 18:11:09
+ * @LastEditors: lby
+ */
 #include <iostream>
 int main()
 {
-using namespace std;
-int a, b;
-cout << "Enter two integers: ";
-cin >> a >> b;
-cout << "The larger of " << a << " and " << b;
-int c = a > b ? a : b; // c = a if a > b, else c = b
-cout << " is " << c << endl;
-return 0;
+  using namespace std;
+  int a, b;
+  cout << "Enter two integers: ";
+  cin >> a >> b;
+  cout << "The larger of " << a << " and " << b;
+  int c = a > b ? a : b;
+  cout << " is " << c << endl;
+  return 0;
 }
