a335ce6a0fe6fad450fab1e641009d65514dc99e
littlebugyang <im.lby@qq.com>
2021-03-14 21:55:13 +0800

    Talking about class constructions and destructors, add stock10.h, stock10.cpp, and usestok1.cpp.

---
+++ cpp_primer_plus/chapter10/10.4_stock10.h
@@ -0,0 +1,26 @@
+/*
+ * @Date: 2021-03-14 20:35:29
+ * @LastEditors: lby
+ */
+#ifndef STOCK10_H_
+#define STOCK01_H_
+#include <string>
+class Stock
+{
+private:
+  std::string company;
+  long shares;
+  double share_val;
+  double total_val;
+  void set_tot() { total_val = shares * share_val; }
+
+public:
+  Stock();
+  Stock(const std::string &co, long n = 0, double pr = 0.0);
+  ~Stock();
+  void buy(long num, double price);
+  void sell(long num, double price);
+  void update(double price);
+  void show();
+};
+#endif
