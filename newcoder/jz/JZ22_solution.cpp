c97743b23b400b80a4f602c86275eb34fdace67b
littlebugyang <im.lby@qq.com>
2021-04-26 11:42:50 +0800

    Modify file location.

--- newcoder/jz/JZ22_solution.cpp
+++ newcoder/jz/JZ22_solution.cpp
@@ -12,48 +12,6 @@ struct TreeNode
 class Solution
 {
 public:
-    struct Queue
-    {
-        struct TreeNode *treeNode;
-        struct Queue *next;
-        Queue() : treeNode(nullptr), next(nullptr) {}
-    };
-    vector<int> PrintFromTopToBottom2(TreeNode *root)
-    {
-        vector<int> result;
-        Queue queue;
-        queue.next = new Queue();
-        queue.next->treeNode = root;
-        Queue *current = queue.next;
-
-        while (queue.next != nullptr)
-        {
-            // 1. 加入队首元素的左右孩子
-            if (queue.next->treeNode->left != nullptr)
-            {
-                current->next = new Queue();
-                current = current->next;
-                current->treeNode = queue.next->treeNode->left;
-            }
-            if (queue.next->treeNode->right != nullptr)
-            {
-                current->next = new Queue();
-                current = current->next;
-                current->treeNode = queue.next->treeNode->right;
-            }
-
-            // 2. 访问队首元素的值
-            result.push_back(queue.next->treeNode->val);
-
-            // 3. 出队
-            Queue *temp = queue.next;
-            queue.next = queue.next->next;
-            delete temp;
-        }
-        delete queue.next;
-        return result;
-    }
-
     vector<int> PrintFromTopToBottom(TreeNode *root)
     {
         vector<TreeNode *> queue;
