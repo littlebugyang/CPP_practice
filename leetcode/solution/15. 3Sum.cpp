dbb88301d0f2eb4a12b51f0bf4012b3e30d84033
littlebugyang <im.lby@qq.com>
2021-05-05 23:59:17 +0800

    Finish 15. Leave 426 unsolved.

--- leetcode/solution/15. 3Sum.cpp
+++ leetcode/solution/15. 3Sum.cpp
@@ -1,7 +1,5 @@
 #include <vector>
-#include <map>
 #include <algorithm>
-using std::map;
 using std::sort;
 using std::vector;
 
@@ -11,13 +9,37 @@ public:
     vector<vector<int>> threeSum(vector<int> &nums)
     {
         vector<vector<int>> result;
-        if (nums.size() == 0)
+        if (nums.size() < 3)
             return result;
 
-        map<int, int> numsMap;
         sort(nums.begin(), nums.end());
+
         for (int i = 0; i < nums.size() - 2; ++i)
         {
+            int j = i + 1, k = nums.size() - 1;
+            vector<int> temp{nums[i]};
+            while (j < k)
+            {
+                if (nums[i] + nums[j] + nums[k] == 0)
+                {
+                    temp.push_back(nums[j]);
+                    temp.push_back(nums[k]);
+                    result.push_back(temp);
+                    temp.pop_back();
+                    temp.pop_back();
+                    while (j + 1 < k && nums[j + 1] == nums[j])
+                        ++j;
+                    while (j < k - 1 && nums[k - 1] == nums[k])
+                        --k;
+                }
+                if (nums[j] + nums[k] < -nums[i])
+                    ++j;
+                else
+                    --k;
+            }
+            while (i + 1 < nums.size() && nums[i + 1] == nums[i])
+                ++i;
         }
+        return result;
     }
 };
---
+++ leetcode/solution/426. Convert Binary Search Tree to Sorted Doubly Linked List.cpp
@@ -0,0 +1,31 @@
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+
+    Node() {}
+
+    Node(int _val)
+    {
+        val = _val;
+        left = nullptr;
+        right = nullptr;
+    }
+
+    Node(int _val, Node *_left, Node *_right)
+    {
+        val = _val;
+        left = _left;
+        right = _right;
+    }
+};
+
+class Solution
+{
+public:
+    Node *treeToDoublyList(Node *root)
+    {
+    }
+};
